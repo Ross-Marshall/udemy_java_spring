434882fb9359244807d3fc8b270cb213e164a2d6
Ross-Marshall <rossmarshallgithub@gmail.com>
2016-05-28 09:01:37 -0400

    Add files via upload

---
+++ spring-tutorial-retest-1/src/main/java/com/caveofprogramming/spring/retest/App.java
@@ -0,0 +1,10 @@
+package com.caveofprogramming.spring.retest;
+
+public class App {
+	
+	public static void main(String [] args ) {
+		Person person = new Person();
+		person.speak();
+	}
+
+}
---
+++ spring-tutorial-retest-1/src/main/java/com/caveofprogramming/spring/retest/Person.java
@@ -0,0 +1,7 @@
+package com.caveofprogramming.spring.retest;
+
+public class Person {
+     public void speak() {
+    	 System.out.println( "Hello! I'm another person");
+     }
+}
