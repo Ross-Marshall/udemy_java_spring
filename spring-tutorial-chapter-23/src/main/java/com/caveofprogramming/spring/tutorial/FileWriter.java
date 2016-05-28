031cba696d0e500588e34ad01b5e35a5cb5424b1
Ross-Marshall <rossmarshallgithub@gmail.com>
2016-05-28 09:12:01 -0400

    Add files via upload

---
+++ spring-tutorial-chapter-23/src/main/java/com/caveofprogramming/spring/tutorial/FileWriter.java
@@ -0,0 +1,10 @@
+package com.caveofprogramming.spring.tutorial;
+
+public class FileWriter implements LogWriter {
+
+	public void write(String text) {
+		System.out.println( "Write to file: " + text );
+		
+	}
+
+}
