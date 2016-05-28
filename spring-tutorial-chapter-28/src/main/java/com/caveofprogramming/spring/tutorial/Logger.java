de820c1e3f5279ee8fd8e092310465a6f54ef451
Ross-Marshall <rossmarshallgithub@gmail.com>
2016-05-28 09:14:29 -0400

    Add files via upload

---
+++ spring-tutorial-chapter-28/src/main/java/com/caveofprogramming/spring/tutorial/Logger.java
@@ -0,0 +1,32 @@
+package com.caveofprogramming.spring.tutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.portlet.bind.annotation.ResourceMapping;
+import javax.annotation.*;
+
+public class Logger {
+	
+	private ConsoleWriter consoleWriter;
+	private LogWriter fileWriter;
+	
+	@Resource
+	public void setConsoleWriter(ConsoleWriter consoleWriter) {
+		this.consoleWriter = consoleWriter;
+	}
+	
+	@Resource(name="squirrel")
+	public void setFileWriter(LogWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+
+	public void writeFile( String text ) {
+		fileWriter.write(text);
+	}
+	
+	public void writeConsole( String text ) {
+		if ( consoleWriter == null ) {
+		    consoleWriter.write(text);
+		}
+	}
+}
