81f137d283bfb07478682f6cc14cc6decb96689d
Ross-Marshall <rossmarshallgithub@gmail.com>
2016-05-28 09:17:05 -0400

    Add files via upload

---
+++ spring-tutorial-chapter-34/src/main/java/com/caveofprogramming/spring/tutorial/RandomText.java
@@ -0,0 +1,22 @@
+package com.caveofprogramming.spring.tutorial;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomText {
+	
+	private static String texts [] = {
+		"I'll be back",
+		"Get Out!",
+		"I want your clothes, boots and motorcycle",
+		null
+	};
+	
+	public String getText() {
+		Random random = new Random();
+		
+		return texts[ random.nextInt( texts.length ) ];
+	} 
+}
---
+++ spring-tutorial-chapter-34/src/main/java/com/caveofprogramming/spring/tutorial/Robot.java
@@ -0,0 +1,27 @@
+package com.caveofprogramming.spring.tutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Robot {
+	
+private String id = "Default robot";
+private String speech = "hello...";
+
+
+	public void speak() {
+		System.out.println( id + " : " + speech);
+	}
+
+	@Autowired
+	public void setId(@Value("#{randomText.text?.length()}") String id) {
+		this.id = id;
+	}
+
+	@Autowired
+	public void setSpeech(@Value("#{randomText.getText()}") String speech) {
+		this.speech = speech;
+	}
+}
