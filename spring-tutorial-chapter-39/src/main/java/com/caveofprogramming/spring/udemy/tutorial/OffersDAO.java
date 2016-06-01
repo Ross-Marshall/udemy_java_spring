ea882ad3dea0e6cd44d60aaa7dd40b4246a13f84
Ross-Marshall <rossmarshallgithub@gmail.com>
2016-05-31 20:15:33 -0400

    Add files via upload

---
+++ spring-tutorial-chapter-39/src/main/java/com/caveofprogramming/spring/udemy/tutorial/OffersDAO.java
@@ -0,0 +1,12 @@
+package com.caveofprogramming.spring.udemy.tutorial;
+
+import java.util.List;
+
+public class OffersDAO {
+	
+	public List <Offer> getOffers() {
+		return null;
+		
+	}
+
+}
---
+++ spring-tutorial-chapter-39/src/main/java/com/caveofprogramming/spring/udemy/tutorial/Robot.java
@@ -0,0 +1,28 @@
+package com.caveofprogramming.spring.udemy.tutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+//Google SPL Spring
+
+@Component
+public class Robot {
+
+	private String id = "Default robot";
+	private String speech = "hello...";
+
+	public void speak() {
+		System.out.println(id + " : " + speech);
+	}
+
+	//@Autowired	
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	//@Autowired
+	public void setSpeech(String speech) {
+		this.speech = speech;
+	}
+}
